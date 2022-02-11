654c915811b4868d4ae7d1d7ac069fad413a3eee
masuibass <masuibass@gmail.com>
2022-02-11 18:36:58 +0900

    Install dependencies and initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      lineClamp: {
+        7: "7",
+        8: "8",
+        9: "9",
+        10: "10",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
