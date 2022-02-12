afda6b9a2a3f806630e569ac263ca6949835c4c1
masuibass <masuibass@gmail.com>
2022-02-13 01:32:37 +0900

    add action

--- app/cardsSlice.ts
+++ app/cardsSlice.ts
@@ -1,32 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { CardProps } from "../components/Card";
-
-const defaultCardsList: CardProps[] = [
-  {
-    id: 1,
-    sdgs_goal_id: 1,
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-    author: "author a",
-    position: { x: 100, y: 200 },
-  },
-  {
-    id: 2,
-    sdgs_goal_id: 2,
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-    author: "author b",
-    position: { x: 300, y: 100 },
-  },
-  {
-    id: 3,
-    sdgs_goal_id: 3,
-    comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-    author: "author c",
-    position: { x: 400, y: 300 },
-  },
-];
+import { CardProps } from "../components/DraggableCard";
 
 interface CardsState {
   list: CardProps[];
@@ -34,8 +7,39 @@ interface CardsState {
 
 interface MoveCardPayload {
   position: { x: number; y: number };
-  id: number;
+  id: string;
 }
+
+interface RemoveCardPayload {
+  id: string;
+}
+
+const defaultCardsList: CardProps[] = [
+  {
+    id: "1e71cd67-6f7b-4ee2-9a9e-912cacf46e95",
+    sdgs_goal_id: 1,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "author a",
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: "e4825c89-ab6c-4159-add1-dca3af0d8dad",
+    sdgs_goal_id: 2,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "author b",
+    position: { x: 200, y: 0 },
+  },
+  {
+    id: "60ec2636-d226-470a-9f3b-a951d43428d8",
+    sdgs_goal_id: 3,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    author: "author c",
+    position: { x: -200, y: 0 },
+  },
+];
 
 const initialState: CardsState = {
   list: defaultCardsList,
@@ -59,8 +63,13 @@ export const counterSlice = createSlice({
         },
       };
     },
+    remove: (state, action: PayloadAction<RemoveCardPayload>) => {
+      const { id } = action.payload;
+      const targetCardIndex = state.list.findIndex((card) => card.id == id);
+      state.list.splice(targetCardIndex, 1);
+    },
   },
 });
 
-export const { add, move } = counterSlice.actions;
+export const { add, move, remove } = counterSlice.actions;
 export default counterSlice.reducer;
